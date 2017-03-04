a928c97113a8f9050583a8ef86d75d0bcf032228
pavan reddy <pavan reddy>
2017-03-04 08:14:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ravinderreddy/sharedloginsingup/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.example.ravinderreddy.sharedloginsingup;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Ravinder Reddy on 04-03-2017.
+ */
+public class SignupActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+    }
+}
